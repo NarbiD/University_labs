71eab18d8554d6afc5828886d6c5b4fdf97e8a6b
NarbiD <ignashov.denis@gmail.com>
2016-10-23 23:53:55 +0300

    Added fixed-point method and Newton's method
    Added some methods.

--- Numerical Methods/Nonlinear equations/src/Nonlinear_equations/Method.java
+++ Numerical Methods/Nonlinear equations/src/Nonlinear_equations/Method.java
@@ -1,38 +1,80 @@
 package Nonlinear_equations;
 
-import java.lang.*;
-
 /**
  * Created by NarbiD on 14.10.2016.
  */
 
 public class Method {
 
-    private final double eps = 0.0000001;
+    private static final double eps = 0.0000001;
 
-    private double equation(double x) {
+    private static double equation(double x) {
         return x*x*x - 3*x*x - 17*x + 22;
     }
+    private static double derivative(double x) {
+        return 3*x*x - 6*x - 17;
+    }
 
-    public double Dichotomy(double left, double right) {
+    public static double Dichotomy(double left, double right) {
         System.out.println("Метод дихотомії(бінарний пошук):");
 
         if (Math.signum(equation(left)) == Math.signum(equation(right))) {
             System.out.println("Немає розв'язків");
-            return Integer.MIN_VALUE;
+            //return Integer.MIN_VALUE;
         }
 
-        double mid = left + (right - left) / 2;
-        for(int i = 0; Math.abs(right - left) > eps; ++i) {
-            System.out.println("Ітерація " + i);
-            if (equation(mid) > 0) {
+        double mid = (left + right) / 2.0;
+        for(int i = 1; Math.abs(right - left) > eps; ++i) {
+            System.out.println("Ітерація " + i + ", " + mid);
+            if (equation(mid) > 0.0) {
                 right = mid;
             } else {
                 left = mid;
             }
-            mid = left + (right - left) / 2;
+            mid = (left + right) / 2.0;
         }
         System.out.println(mid);
         return mid;
+    }
+
+    public static double FixedPointIteration(double left, double right) {
+        System.out.println("Метод простої ітерації:");
+        double x1 = (left + right) / 2.0;
+        double lambda = 0.5 * Math.signum(derivative(x1));
+        if (Math.abs(1.0 - lambda * derivative(x1)) >= 1) {
+            System.out.println("Немає розв'язків");
+            //return Integer.MIN_VALUE;
+        }
+        double x2;
+        x2 = x1;
+        x1 = x2 - 2.0 * eps;
+        for(int i = 1; Math.abs(x1 - x2) > eps; ++i) {
+            x1 = x2;
+            System.out.println("Ітерація " + (i) + ", " + x1);
+            x2 -= lambda * equation(x1);
+        }
+        System.out.println(x2);
+        return x2;
+    }
+
+    public static double Newton(double left, double right) {
+        System.out.println("Метод Ньютона:");
+        double x1 = (left + right) / 2.0;
+        double lambda = -1.0 * Math.signum(derivative(x1));
+        if (Math.abs(1.0 - lambda * derivative(x1)) >= 1) {
+            System.out.println("Немає розв'язків");
+            //return Integer.MIN_VALUE;
+        }
+        double x2;
+        x2 = x1;
+        x1 = x2 - 2.0 * eps;
+        for(int i = 1; Math.abs(x1 - x2) > eps; ++i) {
+            x1 = x2;
+            System.out.println("Ітерація " + (i) + ", " + x1);
+            lambda = -1.0 * Math.signum(derivative(x2));
+            x2 -= lambda * equation(x1);
+        }
+        System.out.println(x2);
+        return x2;
     }
 }
