f13949c59a179fbfc0be0a02c32df5c76e960567
NarbiD <ignashov.denis@gmail.com>
2016-12-21 18:25:43 +0200

    Fix bugs
    Fix bugs in lab 1 (Nolinear equations)

--- Numerical Methods/Nonlinear equations/src/Nonlinear_equations/Main.java
+++ Numerical Methods/Nonlinear equations/src/Nonlinear_equations/Main.java
@@ -3,9 +3,8 @@ package Nonlinear_equations;
 public class Main {
 
     public static void main(String[] args) {
-	// TODO: the main program
-        Method.Dichotomy(1,2);
+        //Method.Dichotomy(1,2);
         //Method.FixedPointIteration(5,6);
-        //Method.Newton(5,6);
+        Method.Newton(5,6);
     }
 }
--- Numerical Methods/Nonlinear equations/src/Nonlinear_equations/Method.java
+++ Numerical Methods/Nonlinear equations/src/Nonlinear_equations/Method.java
@@ -20,13 +20,12 @@ public class Method {
 
         if (Math.signum(equation(left)) == Math.signum(equation(right))) {
             System.out.println("Немає розв'язків");
-            //return Integer.MIN_VALUE;
         }
 
         double mid = (left + right) / 2.0;
         for(int i = 1; Math.abs(right - left) > eps; ++i) {
             System.out.println("Ітерація " + i + ", " + mid);
-            if (equation(mid) > 0.0) {
+            if (equation(mid)*equation(left) < 0.0) {
                 right = mid;
             } else {
                 left = mid;
@@ -40,10 +39,9 @@ public class Method {
     public static double FixedPointIteration(double left, double right) {
         System.out.println("Метод простої ітерації:");
         double x1 = (left + right) / 2.0;
-        double lambda = 0.5 * Math.signum(derivative(x1));
+        double lambda = 0.035 / Math.signum(derivative(x1));
         if (Math.abs(1.0 - lambda * derivative(x1)) >= 1) {
             System.out.println("Немає розв'язків");
-            //return Integer.MIN_VALUE;
         }
         double x2;
         x2 = x1;
@@ -60,10 +58,9 @@ public class Method {
     public static double Newton(double left, double right) {
         System.out.println("Метод Ньютона:");
         double x1 = (left + right) / 2.0;
-        double lambda = -1.0 * Math.signum(derivative(x1));
+        double lambda = 1.0 / derivative(x1);
         if (Math.abs(1.0 - lambda * derivative(x1)) >= 1) {
             System.out.println("Немає розв'язків");
-            //return Integer.MIN_VALUE;
         }
         double x2;
         x2 = x1;
@@ -71,7 +68,7 @@ public class Method {
         for(int i = 1; Math.abs(x1 - x2) > eps; ++i) {
             x1 = x2;
             System.out.println("Ітерація " + (i) + ", " + x1);
-            lambda = -1.0 * Math.signum(derivative(x2));
+            lambda = 1.0 / derivative(x2);
             x2 -= lambda * equation(x1);
         }
         System.out.println(x2);
