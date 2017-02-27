285f0c199ca43c6cb4f2ef1f4a54739cd02c86da
Denis Ignashov <ignashov.denis@gmail.com>
2017-02-27 20:35:24 +0200

    Added graphics for all methods, exept Simpson's method
    Added graphics for some methods and original function. Still need to add
    graphics for Simpson's solve.

--- Numerical Methods/numerical_integration/Integration.py
+++ Numerical Methods/numerical_integration/Integration.py
@@ -1,49 +1,71 @@
-from math import fabs
+from numpy import fabs, arange, sin, pi
+import matplotlib.pyplot as plt
 
 
-def function_f(x):
-    return x*x*x + 3.0*x - 2.0
+class Function:
+    domain = arange(0, pi, 0.1)
+    codomain = list()
+    calculate = None
+
+    def __init__(self, func):
+        self.calculate = func
+
+    def build_plot(self):
+        self.codomain = [self.calculate(x) for x in self.domain]
+        plt.plot(self.domain, self.codomain, linewidth=2.0, label="Original function")
 
 
 class Integration:
     EPS = 1e-6
 
     @staticmethod
-    def rectangles(a, b, iterations=100):
+    def rectangles(func, a, b, iterations=100):
         delta_x = (b - a) / iterations
-        x = a + 0.5 * delta_x
+        x = [a + 0.5 * delta_x]
+        y = [func(x[0])]
         current_area = 0.0
         for iteration in range(iterations):
             prev_area = current_area
-            current_area += function_f(x) * delta_x
+            current_area += func(x[iteration]) * delta_x
             if fabs(current_area - prev_area) < Integration.EPS:
                 break
-            x += delta_x
+            x.append(x[iteration] + delta_x)
+            y.append(func(x[iteration+1]))
+        plt.plot(x, y, 'bs', color='red', label='Rectangles')
         return current_area
 
     @staticmethod
-    def trapezoids(a, b, iterations=200):
+    def trapezoids(func, a, b, iterations=200):
         delta_x = (b - a) / iterations
+        x = [a + 0.5 * delta_x]
+        y = [func(x[0])]
         current_area = 0.0
-        for iteration in range(1, iterations):
-            x = a + iteration * delta_x
-            current_area += function_f(x)
-        return delta_x * ((function_f(a) + function_f(b)) / 2 + current_area)
+        for iteration in range(0, iterations):
+            current_area += func(x[iteration])
+            x.append(x[iteration] + delta_x)
+            y.append(func(x[iteration + 1]))
+        plt.plot(x, y, 'g^', color='yellow', label='Rectangles')
+        return delta_x * ((func(a) + func(b)) / 2 + current_area)
 
     @staticmethod
-    def simpson(a, b, iterations=200):
+    def simpson(func, a, b, iterations=200):
         delta_x = (b - a) / iterations
         x = a
-        current_area = function_f(a) + function_f(b)
+        current_area = func(a) + func(b)
         for iteration in range(1, iterations):
             x += delta_x
             if iteration % 2:
-                current_area += 2.0 * function_f(x)
+                current_area += 2.0 * func(x)
             else:
-                current_area += 4.0 * function_f(x)
+                current_area += 4.0 * func(x)
         return (delta_x/3.0)*current_area
 
 
-print(Integration.rectangles(0, 1))
-print(Integration.trapezoids(0, 1))
-print(Integration.simpson(0, 1))
+func_f = Function(lambda x: sin(x))
+func_f.build_plot()
+
+print(Integration.rectangles(func_f.calculate, 0, pi))
+print(Integration.trapezoids(func_f.calculate, 0, pi))
+print(Integration.simpson(func_f.calculate, 0, pi))
+plt.legend()
+plt.show()
