1b05cdc41d79050506b2387a978cfe996e30df4f
Denis Ignashov <ignashov.denis@gmail.com>
2017-04-02 16:25:53 +0300

    Added area chart
    Added area chart
    The program is divided into several files

--- Numerical Methods/numerical_integration/Integration.py
+++ Numerical Methods/numerical_integration/Integration.py
@@ -1,25 +1,14 @@
-from numpy import fabs, arange, sin, pi
-import matplotlib.pyplot as plt
-
-
-class Function:
-    domain = arange(0, pi, 0.1)
-    codomain = list()
-    calculate = None
-
-    def __init__(self, func):
-        self.calculate = func
-
-    def build_plot(self):
-        self.codomain = [self.calculate(x) for x in self.domain]
-        plt.plot(self.domain, self.codomain, linewidth=2.0, label="Original function")
+from numpy import fabs, arange
 
 
 class Integration:
     EPS = 1e-6
 
+    area_domain = list()
+    area_codomain = list()
+
     @staticmethod
-    def rectangles(func, a, b, iterations=100):
+    def rectangles(func, a, b, iterations=180):
         delta_x = (b - a) / iterations
         x = [a + 0.5 * delta_x]
         y = [func(x[0])]
@@ -31,41 +20,34 @@ class Integration:
                 break
             x.append(x[iteration] + delta_x)
             y.append(func(x[iteration+1]))
-        plt.plot(x, y, 'bs', color='red', label='Rectangles')
+        # plt.plot(x, y, 'bs', color='red', label='Rectangles')
+
         return current_area
 
     @staticmethod
-    def trapezoids(func, a, b, iterations=200):
+    def trapezoids(func, a, b, iterations=150):
         delta_x = (b - a) / iterations
         x = [a + 0.5 * delta_x]
         y = [func(x[0])]
         current_area = 0.0
-        for iteration in range(0, iterations):
+        for iteration in range(iterations):
             current_area += func(x[iteration])
             x.append(x[iteration] + delta_x)
             y.append(func(x[iteration + 1]))
-        plt.plot(x, y, 'g^', color='yellow', label='Rectangles')
+        # plt.plot(x, y, 'g^', color='yellow', label='Trapezoids')
         return delta_x * ((func(a) + func(b)) / 2 + current_area)
 
     @staticmethod
-    def simpson(func, a, b, iterations=200):
+    def simpson(func, a, b, iterations=50):
         delta_x = (b - a) / iterations
+        Integration.area_domain.extend(arange(a, b, delta_x))
         x = a
         current_area = func(a) + func(b)
-        for iteration in range(1, iterations):
+        for iteration in range(iterations):
             x += delta_x
             if iteration % 2:
                 current_area += 2.0 * func(x)
             else:
                 current_area += 4.0 * func(x)
+            Integration.area_codomain.append((delta_x/3)*current_area)
         return (delta_x/3.0)*current_area
-
-
-func_f = Function(lambda x: sin(x))
-func_f.build_plot()
-
-print(Integration.rectangles(func_f.calculate, 0, pi))
-print(Integration.trapezoids(func_f.calculate, 0, pi))
-print(Integration.simpson(func_f.calculate, 0, pi))
-plt.legend()
-plt.show()
