00d67ad523265aff0213d384a3e4ec0c20eb2275
Denis Ignashov <ignashov.denis@gmail.com>
2017-10-08 14:28:06 +0300

    Added  javafx elements
    - changed version of java (due to compatibility issues)
    - maven-assembly-plugin changed to javafx-maven-plugin
    - added empty window

--- InformationTechnology/LocalDbms/src/main/java/localdbms/Bootstrap.java
+++ InformationTechnology/LocalDbms/src/main/java/localdbms/Bootstrap.java
@@ -1,10 +1,24 @@
 package localdbms;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Bootstrap {
+public class Bootstrap extends Application {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
+        primaryStage.setTitle("");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 }
