30e5fedafe0c2a4dcdd269dd1740faf39bb7f3f7
NarbiD <ignashov.denis@gmail.com>
2018-09-28 10:38:55 +0300

    added interfaces for lab3A(6)

---
+++ MOOP/lab3/src/main/java/A6/Director.java
@@ -0,0 +1,6 @@
+package A6;
+
+public interface Director {
+    void layOff(Employe employe);
+    void raiseWages(Employe employe, double newWage);
+}
---
+++ MOOP/lab3/src/main/java/A6/Employe.java
@@ -0,0 +1,8 @@
+package A6;
+
+public interface Employe {
+    void work();
+    void getAWage(double amount);
+    void goToDinner(int minutes);
+    void goHome();
+}
